eec463f6c78514f126f1c072953b724719f7bfb6
a <a@as-Virtual-Machine.local>
2023-07-16 00:00:20 -0700

    Add command line argument parsing to main function

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,41 @@ module Main where
 
 import Prelude
 
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
+import Options.Applicative (Parser, ParserInfo, argument, execParser, fullDesc, header, helper, info, progDesc, str, (<**>))
+import Options.Applicative.Builder (metavar)
+
+data InstallArgs = InstallArgs
+  { browser :: String
+  , extensionId :: String
+  , script :: String
+  }
+
+derive instance genericInstallArgs :: Generic InstallArgs _
+instance showInstallArgs :: Show InstallArgs where
+  show = genericShow
+
+installArgs :: Parser InstallArgs
+installArgs = map InstallArgs $ { browser: _, extensionId: _, script: _ }
+  <$> argument str (metavar "BROWSER")
+  <*> argument str (metavar "EXTENSION_ID")
+  <*> argument str (metavar "SCRIPT")
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = installExtension =<< execParser opts
+
+opts :: ParserInfo InstallArgs
+opts = info (installArgs <**> helper)
+  ( fullDesc
+      <> progDesc "Install an extension for a specific browser"
+      <> header "extension install - Extension Installer"
+  )
+
+installExtension :: InstallArgs -> Effect Unit
+installExtension (InstallArgs { browser, extensionId, script }) =
+  log $ "Installing extension " <> extensionId <> " for browser " <> browser
+    <> " with script "
+    <> show script

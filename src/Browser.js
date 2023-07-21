76b013d9ff0bc3ef7d5c31a5bee6f649f4df7f99
a <a@as-Virtual-Machine.local>
2023-07-20 17:26:07 -0700

    Add WebSocket connection to Browser.js

--- src/Browser.js
+++ src/Browser.js
@@ -35,5 +35,18 @@ export const getAll = async () => {
 };
 
 export const addListener = () => {
+  // https://developer.mozilla.org/en-US/docs/Web/API/WebSocket#examples
+  // Create WebSocket connection.
+  const socket = new WebSocket("ws://localhost:8080");
+
+  // Connection opened
+  socket.addEventListener("open", (event) => {
+    socket.send("Hello Server!");
+  });
+
+  // Listen for messages
+  socket.addEventListener("message", (event) => {
+    console.log("Message from server ", event.data);
+  });
   return chrome.storage.onChanged.addListener(console.log);
 };

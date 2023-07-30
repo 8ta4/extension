f4cb0f7d9233365d1b004ec36613fae7ba816dc6
a <a@as-MacBook-Pro.local>
2023-07-29 20:13:17 -0700

    Update spago documentation link to point to specific version

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // an issue where 'Dynamic require of "stream" is not supported'.
 // Therefore as a workaround, we're using the 'spago build' command.
 // Invoking the 'spago build' command generates 'index.js' in './output/Main/'.
-// https://github.com/purescript/spago#:~:text=You%20can%20already,main%20with%20Node.
+// https://github.com/purescript/spago/blob/6c2deb244ca8b8ee81016c4be0fcdd7027e77945/README.md?plain=1#L88-L96
 import("./output/Main/index.js").then((m) => m.main());

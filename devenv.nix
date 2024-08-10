56adb69b9412f6c55419cbb210c2edf50ad7bff4
a <a@as-Virtual-Machine.local>
2024-08-10 02:23:39 -0700

    Add spago as a devDependency in package-lock.json

--- devenv.nix
+++ devenv.nix
@@ -12,10 +12,7 @@
 
   # https://devenv.sh/scripts/
   scripts.hello.exec = "echo hello from $GREET";
-  scripts.build.exec = ''
-    ${pkgs.nodePackages.npm}/bin/npm install
-    ${pkgs.spago}/bin/spago build
-  '';
+  scripts.build.exec = "spago build";
   scripts.run.exec = ''
     command="$@"
     spago run -wb "$command"
@@ -24,6 +21,8 @@
   enterShell = ''
     hello
     git --version
+    export PATH="$DEVENV_ROOT/node_modules/.bin:$PATH"
+    npm install
     # TODO: Incorporate dependency installation via enterShell
   '';
 

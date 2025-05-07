795bf9cb5b8848474bafa2c6ba2c33470bffff1d
a <a@as-Virtual-Machine.local>
2025-05-06 18:23:19 -0700

    Define external extension paths for browsers

--- src/main.cljs
+++ src/main.cljs
@@ -1,4 +1,9 @@
 (ns main)
 
+(def external-extension-paths
+  {"arc" "/Library/Application Support/Arc/User Data/External Extensions"
+   "chrome" "/Library/Application Support/Google/Chrome/External Extensions"
+   "edge" "/Library/Application Support/Microsoft Edge/External Extensions"})
+
 (defn main
   [])

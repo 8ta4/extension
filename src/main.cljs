0e7480e7a397433a6105da0f75c5b202417e63e9
a <a@as-Virtual-Machine.local>
2025-05-06 18:24:54 -0700

    Add documentation links for external extension paths

--- src/main.cljs
+++ src/main.cljs
@@ -2,7 +2,9 @@
 
 (def external-extension-paths
   {"arc" "/Library/Application Support/Arc/User Data/External Extensions"
+; https://developer.chrome.com/docs/extensions/mv3/external_extensions/#preference-mac
    "chrome" "/Library/Application Support/Google/Chrome/External Extensions"
+; https://learn.microsoft.com/en-us/microsoft-edge/extensions-chromium/developer-guide/alternate-distribution-options#using-a-preferences-json-file-macos-and-linux
    "edge" "/Library/Application Support/Microsoft Edge/External Extensions"})
 
 (defn main

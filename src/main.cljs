5dd70ca160d172c3b4ce56207a0e06689a55479a
a <a@as-Virtual-Machine.local>
2025-12-07 00:30:51 -0800

    Refactor require to use explicit :refer

--- src/main.cljs
+++ src/main.cljs
@@ -1,33 +1,33 @@
 (ns main
-  (:require ["child_process" :as child-process]
-            ["playwright" :as playwright]
-            [shadow.cljs.modern :refer [js-await]]
-            [cljs-node-io.core :as io]
-            [fs]
-            [os]
-            [path]
-            [app-root-path]))
+  (:require [app-root-path :refer [toString]]
+            [child_process :refer [execSync]]
+            [cljs-node-io.core :refer [make-parents]]
+            [fs :refer [mkdtempSync cpSync]]
+            [os :refer [homedir tmpdir]]
+            [path :refer [join]]
+            [playwright :refer [chromium]]
+            [shadow.cljs.modern :refer [js-await]]))
 
 (def browser-external-extension-paths
-  {"arc" (path/join (os/homedir) "Library/Application Support/Arc/User Data/External Extensions")
+  {"arc" (join (homedir) "Library/Application Support/Arc/User Data/External Extensions")
 ; https://developer.chrome.com/docs/extensions/mv3/external_extensions/#preference-mac
-   "chrome" (path/join (os/homedir) "Library/Application Support/Google/Chrome/External Extensions")
+   "chrome" (join (homedir) "Library/Application Support/Google/Chrome/External Extensions")
 ; https://learn.microsoft.com/en-us/microsoft-edge/extensions-chromium/developer-guide/alternate-distribution-options#using-a-preferences-json-file-macos-and-linux
-   "edge" (path/join (os/homedir) "Library/Application Support/Microsoft Edge/External Extensions")})
+   "edge" (join (homedir) "Library/Application Support/Microsoft Edge/External Extensions")})
 
 (defn get-preference-source-path
   [browser]
-  (path/join (app-root-path/toString) "preferences" (str browser ".json")))
+  (join (toString) "preferences" (str browser ".json")))
 
 (defn get-preference-target-path
   [browser extension-id]
-  (path/join (browser-external-extension-paths browser) (str extension-id ".json")))
+  (join (browser-external-extension-paths browser) (str extension-id ".json")))
 
 (defn install-extension-preference-file
   [browser extension-id]
   (js/console.log (str "Installing " extension-id " for " browser))
-  (io/make-parents (get-preference-target-path browser extension-id))
-  (io/copy (get-preference-source-path browser) (get-preference-target-path browser extension-id)))
+  (make-parents (get-preference-target-path browser extension-id))
+  (cpSync (get-preference-source-path browser) (get-preference-target-path browser extension-id)))
 
 (def browser-app-names
   {"arc" "Arc"
@@ -39,27 +39,23 @@
   (str "osascript -e 'quit app \"" (browser-app-names browser) "\"'"))
 
 (def quit-browser
-  (comp child-process/execSync get-quit-command))
+  (comp execSync get-quit-command))
 
 (def temp-directory
-  (os/tmpdir))
+  (tmpdir))
 
 (def app-temp-directory
-  (fs/mkdtempSync (path/join temp-directory "extension-")))
-
-(defn copy
-  [source target]
-  (fs/cpSync source target (clj->js {:recursive true})))
+  (mkdtempSync (join temp-directory "extension-")))
 
 (def browser-user-data-paths
-  {"arc" (path/join (os/homedir) "Library/Application Support/Arc/User Data")
+  {"arc" (join (homedir) "Library/Application Support/Arc/User Data")
 ; https://chromium.googlesource.com/chromium/src/+/main/docs/user_data_dir.md#:~:text=%5BChrome%5D%20~/Library/Application%20Support/Google/Chrome
-   "chrome" (path/join (os/homedir) "Library/Application Support/Google/Chrome")
-   "edge" (path/join (os/homedir) "Library/Application Support/Microsoft Edge")})
+   "chrome" (join (homedir) "Library/Application Support/Google/Chrome")
+   "edge" (join (homedir) "Library/Application Support/Microsoft Edge")})
 
 (defn clone-user-data
   [browser]
-  (copy (browser-user-data-paths browser) app-temp-directory))
+  (cpSync (browser-user-data-paths browser) app-temp-directory (clj->js {:recursive true})))
 
 (def remote-debugging-port
   "9222")
@@ -69,18 +65,18 @@
   (str "open -a '" (browser-app-names browser) "' --args --remote-debugging-port=" remote-debugging-port " --user-data-dir=" app-temp-directory))
 
 (def launch-browser
-  (comp child-process/execSync get-launch-command))
+  (comp execSync get-launch-command))
 
 (defn connect-browser
   []
-  (playwright/chromium.connectOverCDP (str "http://localhost:" remote-debugging-port)))
+  (.connectOverCDP chromium (str "http://localhost:" remote-debugging-port)))
 
 (defn get-manifest-url
   [id]
   (str "chrome-extension://" id "/manifest.json"))
 
 (def init-path
-  (path/join (app-root-path/toString) "public/js/init.js"))
+  (join (toString) "public/js/init.js"))
 
 (defn load-manifest-page
   [id]
